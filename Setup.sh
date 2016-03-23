2724ce66b2d501e7127f3871e7626b70d08ec07c
Naville <403799106@qq.com>
2016-03-23 04:51:26 +0000

    Fuck Travis #1

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Pulling Latest Trunk"
 git submodule foreach git pull origin master
-pip install colorama >>/dev/null
+pip install colorama
 echo "Building capstone"
 cd capstone && ./make.sh ios  >>/dev/null
 echo "Moving capstone"

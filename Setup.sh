a51a426bd6a33c494a9c2d6b6d00ea413887c8f3
Naville <403799106@qq.com>
2016-03-23 06:25:01 +0000

    Fix Setup

--- Setup.sh
+++ Setup.sh
@@ -8,11 +8,16 @@ echo "Pulling Latest Trunk"
 git submodule foreach git pull origin master
 sudo pip install colorama
 echo "Building capstone"
-cd capstone && ./make.sh ios  >>/dev/null
+cd capstone && ./make.sh ios  && cd ../
 echo "Moving capstone"
-mv libcapstone.a ../
+mv ./capstone/libcapstone.a ./
 echo "Downloading Cycript"
 wget https://cydia.saurik.com/api/latest/3 -O Cycript.zip
+echo "Cleaning old framework"
+rm -rf ./Cycript.framework
 echo "Extracting Cycript"
-unzip Cycript.zip 'Cycript.ios/Cycript.framework/*' -d ./
-mv -f ./Cycript.ios/Cycript.framework ./Cycript.framework
+mkdir CYTMP
+unzip Cycript.zip -d ./CYTMP
+cp -avR ./CYTMP/Cycript.ios/*.framework ./ 
+rm -rf ./CYTMP
+rm -rf ./Cycript.zip

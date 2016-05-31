48c36749e95ce3578e2ce2396d14fea1eabecf87
Naville <403799106@qq.com>
2016-06-01 01:48:37 +0800

    Finish OCMethod Generator Core

--- Tools/ClangPlugin/WTFJH.cpp
+++ Tools/ClangPlugin/WTFJH.cpp
@@ -1,4 +1,4 @@
-//===- Obfuscator.cpp ---------------------------------------------===//
+//===- WTFJH.cpp ---------------------------------------------===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -24,23 +24,10 @@
 #include "clang/AST/Type.h"
 #include "clang/Lex/Lexer.h"
 #include "clang/Lex/Lexer.h"
-#include "llvm/Support/RandomNumberGenerator.h"
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
-#include <random>
-#include <time.h>
-//Setup Random Name Array
-#define ObfuscateStringList {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
-#define ObfuscateListSize 52
-std::string arrayString[] = ObfuscateStringList;
-//End
-
-//todo:
-//clang::ObjCSelectorExpr
-//
-
 
 using namespace clang;
 using namespace std;
@@ -48,11 +35,11 @@ using namespace llvm;
 
 
 
-class ObfuscatorVisitor
-  : public RecursiveASTVisitor<ObfuscatorVisitor> {
+class WTFJHVisitor
+  : public RecursiveASTVisitor<WTFJHVisitor> {
 public:
 
-  ObfuscatorVisitor(ASTContext *Context,SourceManager* Manager)
+  WTFJHVisitor(ASTContext *Context,SourceManager* Manager)
     : Context(Context),Manager(Manager),_rewriter(*Manager,Context->getLangOpts ()){
 
     }
@@ -84,36 +71,68 @@ public:
 
 
         llvm::ArrayRef<clang::ParmVarDecl*> PVD=declaration->parameters();
-        int ArgCounter=1;
         for(llvm::ArrayRef<clang::ParmVarDecl*>::iterator iter=PVD.begin();iter!=PVD.end();++iter)
         {
           const clang::ParmVarDecl* PTI=*iter;
-          llvm::errs()<<"("<<PTI->getOriginalType ().getAsString ()<<")arg"<<ArgCounter<<" ";
-          ArgCounter++;
+          llvm::errs()<<"("<<PTI->getOriginalType ().getAsString ()<<")"<<PTI->getNameAsString () <<" ";
         }
        // llvm::errs()<<declaration->getReturnType().getAsString ()<<" ";
-        llvm::errs()<<"{\n"<<declaration->getReturnType().getAsString ()<<" RetVal=%orig\nif(WTShouldLog){";//Start of Bracket
+        llvm::errs()<<"{\n";//Start of Bracket
+        if(declaration->getReturnType().getAsString ()!="void"){
+
+          llvm::errs()<<declaration->getReturnType().getAsString ()<<" RetVal=%orig\n";
+        }
+
+        llvm::errs() <<"if(WTShouldLog){\n";
+
+        //WTShouldLog Called
+        llvm::errs()<<"  WTInit(@\""<<declaration->getClassInterface()->getObjCRuntimeNameAsString ()<<",@\""<<declaration->getSelector ().getAsString()<<"\");\n";
+        //WTAdd Code
+        for(llvm::ArrayRef<clang::ParmVarDecl*>::iterator iter=PVD.begin();iter!=PVD.end();++iter)
+        {
+          const clang::ParmVarDecl* PTI=*iter;
+          llvm::errs()<<"  WTAdd("<<WrapperGenerator(PTI->getOriginalType ().getAsString (),PTI->getNameAsString ())<<",@\""<<PTI->getNameAsString ()<<"\");\n";
+        }        
 
         
+        if(declaration->getReturnType().getAsString ()!="void"){
 
-        llvm::errs()<<"\n}\n";//End of Bracket
+          llvm::errs()<<"WTReturn("<<WrapperGenerator(declaration->getReturnType().getAsString (),"RetVal")<<");";
+        }
+        llvm::errs()<<"\n  WTSave;\n  WTRelease;\n}\n";//End of Bracket
+        if(declaration->getReturnType().getAsString ()!="void"){
 
-        llvm::errs()<<"\nreturn RetVal;\n}\n%end\n";
-        llvm::errs()<<"\n";
+          llvm::errs()<<"\nreturn RetVal;\n";
+        }
+        llvm::errs()<<"\n}\n%end\n";
         }
             return true;
     }
+    static std::string WrapperGenerator(std::string Type,std::string ArgName){
+      
+      std::ostringstream Wrapper;
+        if(Type=="SInt32"){
+          Wrapper<<"[NSNumber numberWithInt:"<<ArgName<<"]";
+        }
+        else{
+          Wrapper<<ArgName;
+        }
+
+      return Wrapper.str();
+    }
+
+
 
   private:
   ASTContext *Context;
   SourceManager* Manager;
-  typedef clang::RecursiveASTVisitor<ObfuscatorVisitor> Base;
+  typedef clang::RecursiveASTVisitor<WTFJHVisitor> Base;
   clang::Rewriter _rewriter;
 
   };
-class ObfuscatorConsumer : public clang::ASTConsumer {
+class WTFJHConsumer : public clang::ASTConsumer {
 public:
-    explicit ObfuscatorConsumer(ASTContext *Context,SourceManager* Manager): Visitor(Context,Manager) {}
+    explicit WTFJHConsumer(ASTContext *Context,SourceManager* Manager): Visitor(Context,Manager) {}
     virtual bool HandleTopLevelDecl(DeclGroupRef DG) {
         // a DeclGroupRef may have multiple Decls, so we iterate through each one
         for (DeclGroupRef::iterator i = DG.begin(), e = DG.end(); i != e; i++) {
@@ -124,16 +143,16 @@ public:
             }
 private:
   SourceManager *Manager;
-  ObfuscatorVisitor Visitor;
+  WTFJHVisitor Visitor;
 };
 
 
-class ObfuscatorAction : public PluginASTAction {
+class WTFJHAction : public PluginASTAction {
 protected:
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  llvm::StringRef) override {
     llvm::errs()<<"WTFJH----Visiting Headers\n";
-    return llvm::make_unique<ObfuscatorConsumer>(&CI.getASTContext(),&CI.getSourceManager ());
+    return llvm::make_unique<WTFJHConsumer>(&CI.getASTContext(),&CI.getSourceManager ());
   }
 
   bool ParseArgs(const CompilerInstance &CI,
@@ -148,7 +167,7 @@ protected:
 };
 
 
-static FrontendPluginRegistry::Add<ObfuscatorAction> X("-WTFJH", "Generate Modules From Specific Header");
+static FrontendPluginRegistry::Add<WTFJHAction> X("-WTFJH", "Generate Modules From Specific Header");
 
 
 
--- Tools/README.md
+++ Tools/README.md
@@ -5,3 +5,13 @@
 **WIP**
 
 This is a Plugin for Clang That, When Supported A Header, Automatically Generates a Module For It.
+Core is complete(hopefully bug-free), although more transform rules needs to be applied into `WrapperGenerator()` of WTFJH.cpp
+###Usage
+1.	Rename the folder into `WTFJH`
+2.	Put it inside `/PATH/TO/LLVM/SRC/tools/clang/examples`
+3.	Add `add_subdirectory(WTFJH)` To The End Of `/PATH/TO/LLVM/SRC/tools/clang/examples/CMakeLists.txt`
+4.	Re-run CMake
+
+###Todo
+1.	Add Generators for properties of a ObjC Class.
+2.	Add Support For C Functions

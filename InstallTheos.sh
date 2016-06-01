c9037e8738d09765eff640a5080fcc8a5344a14f
Naville <403799106@qq.com>
2016-06-01 14:39:31 +0800

    Revert "....."
    This reverts commit 2d84cdc0d1674bce61209b0e74154b69f1242959.

--- InstallTheos.sh
+++ InstallTheos.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+echo "export THEOS=\"/opt/theos\"" >>~/.bash_profile
+source ~/.bash_profile
 sudo git clone  --recursive https://github.com/theos/theos.git $THEOS
 sudo git clone https://github.com/theos/headers.git $THEOS/Headers
 sudo cp -r $THEOS/Headers/ $THEOS/include
